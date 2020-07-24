35cd4edc005ae7180e58db3cd8ee42b9a366838e
shili <shilinb@outlook.com>
2020-07-24 17:51:25 +0800

    VIP用户微信消费

--- creditcard/src/test/java/com/zyyh/TestCreditCard.java
+++ creditcard/src/test/java/com/zyyh/TestCreditCard.java
@@ -104,6 +104,19 @@ public class TestCreditCard {
         user.computePoint();
         Assert.assertEquals(150,user.getTotalPoint());
     }
-
+    @Test
+    public void test_vip_user_wechat(){
+        //新建用户，非VIP
+        User user=new User(true);
+        //初始化信用卡系统
+        CreditCardSystem system=new CreditCardSystem();
+        //用户在系统注册
+        system.addUser(user);
+        //用户使用微信消费1000
+        user.setPayStrategy(new PayByWeChat(1000));
+        //计算积分
+        user.computePoint();
+        Assert.assertEquals(75,user.getTotalPoint());
+    }
 
 }
